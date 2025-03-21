768af76feaa75c22a873b2e38da5c722d19c88c9
mihir123895 <mp520814@gmail.com>
2025-03-21 13:48:19 +0530

    start with backend

---
+++ server/server.js
@@ -0,0 +1,26 @@
+import express from "express"
+import cors from "cors"
+import "dotenv/config"
+import connectDB from "./configs/mongodb.js"
+import { clerkWebhooks } from "./controllers/webhooks.js"
+
+//initialize express
+const app = express()
+
+//connect db
+connectDB()
+
+
+//middlewares
+app.use(cors())
+
+//routes
+app.get("/",(req,res)=> res.send("Hello I am working"))
+app.post('/clerk',express.json(),clerkWebhooks)
+
+//port
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`)
+})
